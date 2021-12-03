143977b48acf2b9e30992ff1d8e846f95e7657e4
HR <hos.raz1993@gmail.com>
2021-12-03 20:17:25 +0300

    first

---
+++ src/components/Header.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <b-navbar toggleable="md" type="dark" variant="success">
+      <b-navbar-brand href="#" id="rang2">Question & Answer</b-navbar-brand>
+
+      <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+
+      <b-collapse id="nav-collapse" is-nav>
+        <b-navbar-nav>
+          <b-nav-item href="#" active
+            >Question {{ numCorrect }}/{{ numTotal }}</b-nav-item
+          >
+        </b-navbar-nav>
+      </b-collapse>
+    </b-navbar>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ["numTotal", "numCorrect"],
+};
+</script>
+<style scoped>
+.navbar {
+  padding-left: 1rem;
+}
+.nav-link, .navbar-brand{
+  cursor: text;
+}
+</style>
